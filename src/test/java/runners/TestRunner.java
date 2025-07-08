5c467ab2121325b3ef8e6c49d65549466dc54807
akenger <akenger@tci.aero>
2025-07-08 15:48:49 +0300

    2. cihaz sıralı olarak calismaya basladi

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -13,20 +13,38 @@ import utilities.DriverFactory;
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
+    private static ThreadLocal<String> threadDeviceName = new ThreadLocal<>();
+    private static ThreadLocal<String> threadUdid = new ThreadLocal<>();
+    private static ThreadLocal<String> threadPlatformVersion = new ThreadLocal<>();
+    private static ThreadLocal<String> threadSystemPort = new ThreadLocal<>();
+    private static ThreadLocal<String> threadAppiumPort = new ThreadLocal<>();
+
     @Parameters({"deviceName", "udid", "platformVersion", "systemPort", "appiumPort"})
-    @BeforeClass
-    public void setUp(
-            @Optional("defaultDevice") String deviceName,
-            @Optional("emulator-5554") String udid,
-            @Optional("11.0") String platformVersion,
-            @Optional("8200") String systemPort,
-            @Optional("4723") String appiumPort) {
+    @BeforeClass(alwaysRun = true)
+    public void setUp(@Optional("defaultDevice") String deviceName,
+                      @Optional("emulator-5554") String udid,
+                      @Optional("11.0") String platformVersion,
+                      @Optional("8200") String systemPort,
+                      @Optional("4723") String appiumPort) {
+        threadDeviceName.set(deviceName);
+        threadUdid.set(udid);
+        threadPlatformVersion.set(platformVersion);
+        threadSystemPort.set(systemPort);
+        threadAppiumPort.set(appiumPort);
+
+        System.out.println(">>>> Parallel Test Setup: " +
+                "DeviceName=" + deviceName +
+                ", UDID=" + udid +
+                ", PlatformVersion=" + platformVersion +
+                ", SystemPort=" + systemPort +
+                ", AppiumPort=" + appiumPort);
 
         DriverFactory.setDriver(deviceName, udid, platformVersion, systemPort, appiumPort);
     }
 
-    @AfterClass
+    @AfterClass(alwaysRun = true)
     public void tearDown() {
+        System.out.println(">>>> Tearing down device: " + threadUdid.get());
         DriverFactory.quitDriver();
     }
 }
